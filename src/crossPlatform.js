27e266d5690d87a197a497284a3340d29a133f23
Songlin Hou <shou@wpi.edu>
2022-09-30 16:49:07 -0400

    update timeline color

--- src/crossPlatform.js
+++ src/crossPlatform.js
@@ -14,3 +14,17 @@ function isOnIOS() {
 // function isOnIOS() {
 //   return false;
 // }
+
+function iOSversion() {
+  if (/iP(hone|od|ad)/.test(navigator.platform)) {
+    // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+    var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+    return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+  }
+  return [-1, -1, -1];
+}
+
+function isAtLeastiOS5(){
+  let ver = iOSversion();
+  return ver[0] >= 5;
+}
