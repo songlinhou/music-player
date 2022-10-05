4a8689c84d8d8e72c165de7a048cb33c3f106199
Songlin Hou <shou@wpi.edu>
2022-10-05 13:08:07 -0400

    add volume control

--- src/utils.js
+++ src/utils.js
@@ -25,3 +25,73 @@ function getTimestampNow(){
     }
     return time;
 }
+
+function getPrefixedVolume(lastVol){
+    if(lastVol < 25){
+        return 25;
+    }
+    if(lastVol < 50){
+        return 50;
+    }
+    if(lastVol < 75){
+        return 75;
+    }
+    if(lastVol < 100){
+        return 100;
+    }
+    if(lastVol == 100){
+        return 0;
+    }
+}
+
+let volumeIcons = [
+    "fa-volume-xmark",
+    "fa-volume-off",
+    "fa-volume-low",
+    "fa-volume-high",
+];
+
+
+function changeVolumeIcon(volume){
+    volumeIcons.forEach(icon => {
+        $("#main-play-volume-btn").removeClass(icon);
+    });
+    $("#main-play-volume-btn").addClass("fa-volume-high"); // to determine the size of slider
+    let minSize = $("#volume-control-player-main").width();
+    let idx = 0;
+    if(volume == 0){
+        idx = 0;
+    }
+    else if(volume <= 25){
+        idx = 1;
+    }
+    else if(volume <= 75){
+        idx = 2;
+    }
+    else if(volume <= 100){
+        idx = 3;
+    }
+    $("#main-play-volume-btn").removeClass("fa-volume-high");
+    $("#main-play-volume-btn").addClass(volumeIcons[idx]);
+    $("#volume-control-player-main").css("width", minSize + "px");
+}
+
+function fixPlayerResize(){
+    function debounce(func){
+        var timer;
+        return function(event){
+          if(timer) clearTimeout(timer);
+          timer = setTimeout(func,100,event);
+        };
+    }
+    window.addEventListener("resize",debounce(function(e){
+        console.log("end of resizing");
+        if($("#main-play-container").is(":visible")){
+            $(".main-play-horizontal-btn").click();
+            setTimeout(()=>{
+                $("#float-player-left").click();
+                console.log("back to main player");
+            }, 2000);
+        }
+    }));
+}
